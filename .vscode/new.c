d5161a465bcaf61daef79d7ea90f5d7cd6c92172
Bohora1 <sandeshbohora1@gmail.com>
2024-12-20 12:52:33 +0545

    class 12

---
+++ .vscode/new.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+
+    int a;
+    int b;
+    printf("enter any two numbers");
+    scanf("%d %d",&a,&b);
+    int sum = a+b;
+    printf("the sum of a and b is %d",sum)
+}
